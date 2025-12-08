c833e9d44e277895650f7374dbc766aa7d5d3940
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-12-08 13:53:04 -0300

    Atualizado rota da api e adicionado .ico customizado

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://locadora-de-veiculos-2025-api-fbe9fggdfve8eeam.brazilsouth-01.azurewebsites.net',
+  apiUrl:
+    'https://locadora-de-veiculos-2025-api-fbe9fggdfve8eeam.brazilsouth-01.azurewebsites.net/api',
 };
